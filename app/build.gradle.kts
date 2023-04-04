0d144e5ffe5ad6ed6f1af91b276f779624fe5214
CW Larson <11445911+opt05@users.noreply.github.com>
2023-04-04 18:31:24 -0500

    Iterated version code for next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     defaultConfig {
         minSdk = 21
         targetSdk = 33
-        versionCode = 16
-        versionName = "1.4.3"
+        versionCode = 17
+        versionName = "1.5.0"
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "com.cwlarson.deviceid.CustomTestRunner"
     }
